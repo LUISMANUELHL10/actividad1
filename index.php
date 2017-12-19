653924a7e8c962968b7ab73283e8e107a8cf55ed
luismanuelhl7 <lmanuel_hernandez@prodigy.net.mx>
2017-12-18 23:09:11 -0600

    commit

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+$api_key = getenv('OPEN_WEATHER_API');
 
 $app = new Silex\Application();
 
-$app->get('/', function() use($app) {
+$app->get('/', function() use($app,$api_key) {
 
-$arreglo = ["alumno: "=>["luis Manuel Hernandez Luna" => "413129145"]];
 
-    return $app->json($arreglo);
-    
+  $client = new Client();
+  $url = "http://api.openweathermap.org/data/2.5/weather?id=3530597&appid=$api_key&units=metric";
+  $response= $client->get($url);
+  $body = $response->getbody();
+
+ 
+  return new Response($body,200,array('Content-Type' => 'application/json'));
+
+
+
+
+
 });
 
 $app->run();
+
+
+
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,9 @@ class ComposerStaticInit12368583a9fcb454e49155f428ac6c5f
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '154e0d165f5fe76e8e9695179d0a7345' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -24,7 +27,14 @@ class ComposerStaticInit12368583a9fcb454e49155f428ac6c5f
         'P' => 
         array (
             'Psr\\Log\\' => 8,
+            'Psr\\Http\\Message\\' => 17,
             'Psr\\Container\\' => 14,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
         ),
     );
 
@@ -61,9 +71,25 @@ class ComposerStaticInit12368583a9fcb454e49155f428ac6c5f
         array (
             0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
         ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
         'Psr\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
     );
 
